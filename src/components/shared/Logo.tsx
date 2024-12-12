6b87ad589a55a49e098a9366b533bb049f91d3ee
elevateher1 <info@elevateher1.org>
2024-12-12 13:16:46 +0300

    new

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import logo from "./logo .png"
 export function Logo() {
   return (
     <div className="flex items-center space-x-2">
       <img 
-        src="/logo.png"
+        src={logo}
         alt="Elevate Her Logo" 
         className="h-10 w-auto"
       />
